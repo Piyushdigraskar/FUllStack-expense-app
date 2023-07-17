1ddd4d575c86ba7c4815086ff9c90691c67dcae8
Piyushdigraskar <piyushdigraskar3@gmail.com>
2023-07-17 10:09:02 +0530

    adding transaction function

--- controllers/expense.js
+++ controllers/expense.js
@@ -1,58 +1,109 @@
-
 const Expense = require('../models/expense');
+const User = require('../models/user');
+const sequalize = require('../util/database');
 
 
-const addUser =  async (req,res,next)=>{
-    try{
-        const name= req.body.name;
-        const selling = req.body.selling;
-        const Category = req.body.Category;
+const addexpense = async (req, res) => {
+    let t;
+    try {
+        t = await sequalize.transaction();
+        const { selling, name, Category } = req.body;
 
-        const data = await Expense.create({name:name, selling:selling, Category:Category});
-        res.status(201).json({data:data});
-    }catch(err){
-        res.status(500).json({
-            error:err
-        })
-    }
-
-}
-
-const getUser =  async (req,res,next)=>{
-    try{
-        const users =await Expense.findAll();
-        res.status(201).json({allUsers:users});
-    }catch(err){
-        console.log('get-user is failing', JSON.stringify(err));
-        res.status(500).json({
-            error:err
-        })
-    }
-    
-}
-
-const deleteUser =  async (req,res)=>{
-    try{
-        if(!req.params.id){
-            console.log('Id is missing');
-            return res.status(400).json({err: 'Id is misssing'});
+        if (selling == undefined || selling.length === 0) {
+            return res
+                .status(400)
+                .json({ success: false, message: 'Parameters missing' });
         }
-        const uid = req.params.id;
-        console.log("user id ==",uid);
-        await Expense.destroy({where: {id:uid}});
-        res.sendStatus(200);
-        
-    }
-    catch(err){
+
+        const expense = await Expense.create(
+            { selling, name, Category, userId: req.user.id },
+            { transaction: t }
+        );
+
+        const totalExpense = Number(req.user.totalExpenses) + Number(selling);
+
+        await User.update(
+            { totalExpenses: totalExpense },
+            { where: { id: req.user.id }, transaction: t }
+        );
+
+        await t.commit();
+        return res.status(200).json({ expense });
+    } catch (err) {
+        if (t) await t.rollback();
         console.log(err);
-        res.status(500).json({
-            error:err
+        return res.status(501).json({ success: false, error: err });
+    }
+};
+
+
+
+
+
+
+const getexpenses = (req, res) => {
+
+    Expense.findAll({ where: { userId: req.user.id } }).then(expenses => {
+        return res.status(200).json({ expenses, success: true })
+    })
+        .catch(err => {
+            console.log(err)
+            return res.status(500).json({ error: err, success: false })
         })
-    }  
-    
 }
-module.exports ={
-    addUser,
-    getUser,
-    deleteUser
+
+// const deleteexpense = (req, res) => {
+//     const expenseid = req.params.expenseid;
+//     const t = await sequalize.transaction();
+//     if (expenseid == undefined || expenseid.length === 0) {
+//         return res.status(400).json({ success: false, })
+//     }
+//     Expense.destroy({ where: { id: expenseid, userId: req.user.id }}, {transaction:t} ).then((noofrows) => {
+//         if (noofrows === 0) {
+//             return res.status(404).json({ success: false, message: 'Expense doenst belong to the user' })
+//         }
+//         await t.commit();
+//         return res.status(200).json({ success: true, message: "Deleted Successfuly" })
+//     }).catch(async (err) => {
+//         await t.rollback();
+//         console.log(err);
+//         return res.status(500).json({ success: true, message: "Failed" })
+//     })
+// }
+const deleteexpense = async (req, res) => {
+    try {
+      const expenseid = req.params.expenseid;
+      const t = await sequalize.transaction();
+  
+      if (expenseid == undefined || expenseid.length === 0) {
+        return res.status(400).json({ success: false });
+      }
+  
+      const noofrows = await Expense.destroy(
+        { where: { id: expenseid, userId: req.user.id }, transaction: t }
+      );
+  
+      if (noofrows === 0) {
+        await t.rollback();
+        return res
+          .status(404)
+          .json({ success: false, message: 'Expense doesn\'t belong to the user' });
+      }
+  
+      await t.commit();
+      return res
+        .status(200)
+        .json({ success: true, message: 'Deleted Successfully' });
+    } catch (err) {
+      if (t) await t.rollback();
+      console.log(err);
+      return res.status(500).json({ success: false, message: 'Failed' });
+    }
+  };
+  
+
+module.exports = {
+    deleteexpense,
+    getexpenses,
+    addexpense
 }
