7c57310dd8a90f3a8b2973fee71099b4a84d5527
Piyushdigraskar <piyushdigraskar3@gmail.com>
2023-07-19 10:02:03 +0530

    changed the expenses

--- controllers/expense.js
+++ controllers/expense.js
@@ -2,54 +2,70 @@ const Expense = require('../models/expense');
 const User = require('../models/user');
 const sequalize = require('../util/database');
 
+const UserServices = require('../services/userservices');
+const S3Service = require('../services/S3services')
+
 
 const addexpense = async (req, res) => {
-    let t;
-    try {
-        t = await sequalize.transaction();
-        const { selling, name, Category } = req.body;
+  const t = await sequalize.transaction();
+  try {
 
-        if (selling == undefined || selling.length === 0) {
-            return res
-                .status(400)
-                .json({ success: false, message: 'Parameters missing' });
-        }
+    const { selling, name, Category } = req.body;
 
-        const expense = await Expense.create(
-            { selling, name, Category, userId: req.user.id },
-            { transaction: t }
-        );
-
-        const totalExpense = Number(req.user.totalExpenses) + Number(selling);
-
-        await User.update(
-            { totalExpenses: totalExpense },
-            { where: { id: req.user.id }, transaction: t }
-        );
-
-        await t.commit();
-        return res.status(200).json({ expense });
-    } catch (err) {
-        if (t) await t.rollback();
-        console.log(err);
-        return res.status(501).json({ success: false, error: err });
+    if (selling == undefined || selling.length === 0) {
+      return res
+        .status(400)
+        .json({ success: false, message: 'Parameters missing' });
     }
+
+    const expense = await Expense.create(
+      { selling, name, Category, userId: req.user.id },
+      { transaction: t }
+    );
+
+    const totalExpense = Number(req.user.totalExpenses) + Number(selling);
+
+    await User.update(
+      { totalExpenses: totalExpense },
+      { where: { id: req.user.id }, transaction: t }
+    );
+
+    await t.commit();
+    return res.status(200).json({ expense });
+  } catch (err) {
+    if (t) await t.rollback();
+    console.log(err);
+    return res.status(501).json({ success: false, error: err });
+  }
 };
 
 
+const downloadexpense = async (req, res) => {
+  try {
+    const expenses = await UserServices.getExpenses(req);
+    console.log(expenses);
+    const stringifiedExpenses = JSON.stringify(expenses);
 
-
+    const userId = req.user.id;
+    const filename = `Expense${userId}/${new Date()}.txt`;
+    const fileURL = await S3Service.uploadToS3(stringifiedExpenses, filename);
+    res.status(200).json({ fileURL, success: true });
+  }
+  catch (err) {
+    console.log(err);
+  }
+}
 
 
 const getexpenses = (req, res) => {
 
-    Expense.findAll({ where: { userId: req.user.id } }).then(expenses => {
-        return res.status(200).json({ expenses, success: true })
+  Expense.findAll({ where: { userId: req.user.id } }).then(expenses => {
+    return res.status(200).json({ expenses, success: true })
+  })
+    .catch(err => {
+      console.log(err)
+      return res.status(500).json({ error: err, success: false })
     })
-        .catch(err => {
-            console.log(err)
-            return res.status(500).json({ error: err, success: false })
-        })
 }
 
 // const deleteexpense = (req, res) => {
@@ -71,39 +87,40 @@ const getexpenses = (req, res) => {
 //     })
 // }
 const deleteexpense = async (req, res) => {
-    try {
-      const expenseid = req.params.expenseid;
-      const t = await sequalize.transaction();
-  
-      if (expenseid == undefined || expenseid.length === 0) {
-        return res.status(400).json({ success: false });
-      }
-  
-      const noofrows = await Expense.destroy(
-        { where: { id: expenseid, userId: req.user.id }, transaction: t }
-      );
-  
-      if (noofrows === 0) {
-        await t.rollback();
-        return res
-          .status(404)
-          .json({ success: false, message: 'Expense doesn\'t belong to the user' });
-      }
-  
-      await t.commit();
-      return res
-        .status(200)
-        .json({ success: true, message: 'Deleted Successfully' });
-    } catch (err) {
-      if (t) await t.rollback();
-      console.log(err);
-      return res.status(500).json({ success: false, message: 'Failed' });
+  const t = await sequalize.transaction();
+  try {
+    const expenseid = req.params.expenseid;
+
+    if (expenseid == undefined || expenseid.length === 0) {
+      return res.status(400).json({ success: false });
     }
-  };
-  
+
+    const noofrows = await Expense.destroy(
+      { where: { id: expenseid, userId: req.user.id }, transaction: t }
+    );
+
+    if (noofrows === 0) {
+      await t.rollback();
+      return res
+        .status(404)
+        .json({ success: false, message: 'Expense doesn\'t belong to the user' });
+    }
+
+    await t.commit();
+    return res
+      .status(200)
+      .json({ success: true, message: 'Deleted Successfully' });
+  } catch (err) {
+    if (t) await t.rollback();
+    console.log(err);
+    return res.status(500).json({ success: false, message: 'Failed' });
+  }
+};
+
 
 module.exports = {
-    deleteexpense,
-    getexpenses,
-    addexpense
+  deleteexpense,
+  getexpenses,
+  addexpense,
+  downloadexpense
 }
